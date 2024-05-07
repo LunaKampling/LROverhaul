578f86e7f8cd5654848db9910d6dd372ff0118c9
Luna Kampling <luna.kampling@gmail.com>
2024-05-07 15:03:07 +0200

    Slightly faster way of traversing linked list for scenery lines
    Starts at end instead of start and traverses backwards. Saves the travel from the front to the first scenery line.
    
    If this does break something (it shouldnt?) you can just uncomment the old code and comment out the new

--- src/Game/EditorCell.cs
+++ src/Game/EditorCell.cs
@@ -7,24 +7,43 @@ namespace linerider
     {
         protected override LinkedListNode<GameLine> FindNodeAfter(LinkedListNode<GameLine> node, GameLine line)
         {
-            if (line.ID < 0)
+            //if (line.ID < 0)
+            //{
+            //    // Scenery lines want to skip right to the beginning of the
+            //    // other scenery line ids
+            //    while (node.Value.ID >= 0)
+            //    {
+            //        node = node.Next;
+            //        if (node == null)
+            //            return null;
+            //    }
+            //}
+            //while (line.ID >= 0
+            //    ? line.ID < node.Value.ID // Physics
+            //    : line.ID > node.Value.ID) // Scenery
+            //{
+            //    node = node.Next;
+            //    if (node == null)
+            //        return null;
+            //}
+            if (line.ID >= 0)
             {
-                // Scenery lines want to skip right to the beginning of the
-                // other scenery line ids
-                while (node.Value.ID >= 0)
+                while (line.ID < node.Value.ID)
                 {
                     node = node.Next;
                     if (node == null)
                         return null;
                 }
             }
-            while (line.ID >= 0
-                ? line.ID < node.Value.ID // Physics
-                : line.ID > node.Value.ID) // Scenery
+            else
             {
-                node = node.Next;
-                if (node == null)
-                    return null;
+                node = node.List.Last;
+                while (line.ID > node.Value.ID)
+                {
+                    node = node.Previous;
+                    if (node == null || node.Value.ID >= 0)
+                        return null;
+                }
             }
             return node;
         }
